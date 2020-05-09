c767984b5d9cc84f2a8882fc55880f1408968419
kondik <kond1k12@gmail.com>
2020-05-09 18:07:01 +0300

    init

---
+++ geekshop/mainapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+
+def main(request):
+    return render(request, 'mainapp/index.html')
+
+
+def products(request):
+    return render(request, 'mainapp/products.html')
+
+
+def contact(request):
+    return render(request, 'mainapp/contact.html')
