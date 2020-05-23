5967a639075bdbd21b03cbe5fe147f2216b288e9
kond1k <kond1k12@gmail.com>
2020-05-23 22:57:23 +0300

    Сделал подсчет цены и товара в корзине

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,6 +1,7 @@
 import datetime
-from .models import ProductCategory, Product, Contact 
+from .models import ProductCategory, Product, Contact
 from django.conf import settings
+import random
 
 from django.shortcuts import render, get_object_or_404
 
@@ -15,11 +16,29 @@ def main(request):
     return render(request, "mainapp/index.html", content)
 
 
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return[]
+
+
+def get_hot_product():
+    products = Product.objects.all()
+    return random.sample(list(products), 1)[0]
+
+
+def get_same_product(hot_product):
+    same_products = Product.objects.filter(
+        category=hot_product.category).exclude(pk=hot_product.pk)[:3]
+    return same_products
+
+
 def products(request, pk=None):
     title = "продукты"
     links_menu = ProductCategory.objects.all()
 
-    basket =[]
+    basket = get_basket(request.user)
     if request.user.is_authenticated:
         basket = Basket.objects.filter(user=request.user)
 
@@ -32,11 +51,12 @@ def products(request, pk=None):
             products = Product.objects.filter(
                 category__pk=pk).order_by("price")
         content = {"title": title, "links_menu": links_menu, "category": category,
-                   "products": products, "media_url": settings.MEDIA_URL, "basket":basket, }
+                   "products": products, "media_url": settings.MEDIA_URL, "basket": basket, }
         return render(request, "mainapp/products_list.html", content)
-    same_products = Product.objects.all()
+    hot_product = get_hot_product()
+    same_products = get_same_product(hot_product)
     content = {"title": title, "links_menu": links_menu,
-               "same_products": same_products, "media_url": settings.MEDIA_URL, }
+               "same_products": same_products, "media_url": settings.MEDIA_URL, "hot_product": hot_product, }
     if pk:
         print(f"User select category: {pk}")
     return render(request, "mainapp/products.html", content)
