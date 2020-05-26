91755f015625ff3adfe0551056fd2b72a141195a
kond1k <kond1k12@gmail.com>
2020-05-26 13:20:01 +0300

    лобваил админку

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from django.contrib import admin
 from django.conf import settings
 from django.conf.urls import include
 from django.conf.urls.static import static
@@ -24,9 +23,9 @@ urlpatterns = [
     path('', mainapp.main, name="main"),
     path('products/', include('mainapp.urls', namespace='products')),
     path('contact/', mainapp.contact, name="contact"),
-    path('admin/', admin.site.urls),
     path('auth/', include('authapp.urls', namespace='auth')),
     path('basket/', include('basketapp.urls', namespace='basket')),
+    path('admin/', include('adminapp.urls', namespace='admin')),
 ]
 
 
