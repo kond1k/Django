98e92aef5b938ea93b038c66b06adf6e9ebdc6bf
kond1k <kond1k12@gmail.com>
2020-05-29 20:55:12 +0300

    add products in adminapp and paginator

--- geekshop/adminapp/views.py
+++ geekshop/adminapp/views.py
@@ -158,23 +158,61 @@ def products(request, pk):
 
 @user_passes_test(lambda u: u.is_superuser)
 def product_create(request, pk):
-    response = redirect("admin:categories")
-    return response
+    title = "продукт/создание"
+    category = get_object_or_404(ProductCategory, pk=pk)
+
+    if request.method == "POST":
+        product_form = ProductEditForm(request.POST, request.FILES)
+        if product_form.is_valid():
+            product_form.save()
+            return HttpResponseRedirect(reverse("admin:products", args=[pk]))
+    else:
+        # set initial value for form
+        product_form = ProductEditForm(initial={"category": category})
+
+    content = {"title": title, "update_form": product_form,
+               "category": category, "media_url": settings.MEDIA_URL}
+    return render(request, "adminapp/product_update.html", content)
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def product_read(request, pk):
-    response = redirect("admin:categories")
-    return response
+class ProductDetailView(LoginRequiredMixin, DetailView):
+    model = Product
+    template_name = "adminapp/product_read.html"
 
 
 @user_passes_test(lambda u: u.is_superuser)
 def product_update(request, pk):
-    response = redirect("admin:categories")
-    return response
+    title = "продукт/редактирование"
+    edit_product = get_object_or_404(Product, pk=pk)
+
+    if request.method == "POST":
+        edit_form = ProductEditForm(
+            request.POST, request.FILES, instance=edit_product)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse("admin:product_update", args=[edit_product.pk]))
+    else:
+        edit_form = ProductEditForm(instance=edit_product)
+
+    content = {
+        "title": title,
+        "update_form": edit_form,
+        "category": edit_product.category,
+        "media_url": settings.MEDIA_URL,
+    }
+    return render(request, "adminapp/product_update.html", content)
 
 
 @user_passes_test(lambda u: u.is_superuser)
 def product_delete(request, pk):
-    response = redirect("admin:categories")
-    return response
+    title = "продукт/удаление"
+    product = get_object_or_404(Product, pk=pk)
+
+    if request.method == "POST":
+        product.is_active = False
+        product.save()
+        return HttpResponseRedirect(reverse("admin:products", args=[product.category.pk]))
+
+    content = {"title": title, "product_to_delete": product,
+               "media_url": settings.MEDIA_URL}
+    return render(request, "adminapp/product_delete.html", content)
