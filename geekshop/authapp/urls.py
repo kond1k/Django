23e0fb98c605b02fb0861bcaf5fbe0622102d8f9
kondik <kond1k12@gmail.com>
2020-06-05 19:03:02 +0300

    add email vertification

--- geekshop/authapp/urls.py
+++ geekshop/authapp/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     re_path(r"^logout/$", authapp.logout, name="logout"),
     re_path(r"^register/$", authapp.register, name="register"),
     re_path(r"^edit/$", authapp.edit, name="edit"),
-    # re_path(r"^verify/(?P<email>.+)/(?P<activation_key>\w+)/$",
-    #         authapp.verify, name="verify"),
+    re_path(r"^verify/(?P<email>.+)/(?P<activation_key>\w+)/$",
+            authapp.verify, name="verify"),
 ]
