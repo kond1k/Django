88a0a00a6c8bcfcc42623e044f3dfb032a6fe2fc
kondik <kond1k12@gmail.com>
2020-06-29 14:32:18 +0300

    after memcached

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -58,7 +58,21 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "debug_toolbar.middleware.DebugToolbarMiddleware",
+    # "django.middleware.cache.FetchFromCacheMiddleware",  # for entire site caching
 ]
+
+CACHE_MIDDLEWARE_ALIAS = "default"
+CACHE_MIDDLEWARE_SECONDS = 120
+CACHE_MIDDLEWARE_KEY_PREFIX = "geekbrains"
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.memcached.MemcachedCache",
+        "LOCATION": "127.0.0.1:11211",
+    }
+}
+
+LOW_CACHE = True
 
 ROOT_URLCONF = 'geekshop.urls'
 
